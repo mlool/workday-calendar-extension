c9629584e9449e8310e15054c4ecd2665b3f6689
Mlool <martinlou2002@gmail.com>
2024-05-27 16:41:42 -0700

    removed unneccessary files

--- src/background/index.ts
+++ src/background/index.ts
@@ -15,20 +15,20 @@ async function incrementStoredValue(tabId: string) {
 }
 
 export async function init() {
-  await storage.local.clear()
+  // await storage.local.clear()
 
-  runtime.onMessage.addListener(async (message: Message) => {
-    if (message.to === 'background') {
-      console.log('background handled: ', message.action)
+  // runtime.onMessage.addListener(async (message: Message) => {
+  //   if (message.to === 'background') {
+  //     console.log('background handled: ', message.action)
 
-      const tab = await getCurrentTab()
-      const tabId = tab.id
+  //     const tab = await getCurrentTab()
+  //     const tabId = tab.id
 
-      if (tabId) {
-        return incrementStoredValue(tabId.toString())
-      }
-    }
-  })
+  //     if (tabId) {
+  //       return incrementStoredValue(tabId.toString())
+  //     }
+  //   }
+  // })
 }
 
 runtime.onInstalled.addListener(() => {
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,29 +1,29 @@
 import { runtime } from 'webextension-polyfill'
 
-console.log('[content] loaded ')
+// console.log('[content] loaded ')
 
-type Listener = (event: MouseEvent) => void
+// type Listener = (event: MouseEvent) => void
 
-let count = 0
+// let count = 0
 
-function registerClickListener(listener: Listener) {
-  window.addEventListener('click', listener)
+// function registerClickListener(listener: Listener) {
+//   window.addEventListener('click', listener)
 
-  // step 2
-  return function cleanup() {
-    window.removeEventListener('click', listener)
-  }
-}
+//   // step 2
+//   return function cleanup() {
+//     window.removeEventListener('click', listener)
+//   }
+// }
 
-async function countClicks() {
-  count++
-  console.log('click(): ', count)
-  // step 2
-  return runtime.sendMessage({ from: 'content', to: 'background', action: 'click' })
-}
+// async function countClicks() {
+//   count++
+//   console.log('click(): ', count)
+//   // step 2
+//   return runtime.sendMessage({ from: 'content', to: 'background', action: 'click' })
+// }
 
-export function init() {
-  registerClickListener(countClicks)
-}
+// export function init() {
+//   registerClickListener(countClicks)
+// }
 
-init()
+// init()
