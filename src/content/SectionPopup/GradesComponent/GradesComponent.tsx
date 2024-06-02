55c858560bc8fa9bbabdd1a4aaf405d04983aa32
StashC <stashubc@student.ubc.ca>
2024-06-01 21:57:11 -0700

    add UBC grades api

---
+++ src/content/SectionPopup/GradesComponent/GradesComponent.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { IGradesAPIData, getGradesData, getGradesUrl } from "./GradesHelper";
+import { ISectionData } from "../../App/App.types";
+import "./GradesComponent.css";
+
+interface IProps {
+  selectedSection: ISectionData;
+}
+
+const GradesComponent = ({ selectedSection }: IProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [gradesData, setGradesData] = useState<IGradesAPIData>();
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchGrades = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        let data = await getGradesData(selectedSection);
+        setGradesData(data);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchGrades();
+  }, [selectedSection]);
+
+  const gradesURL = getGradesUrl(selectedSection);
+
+  return (
+    <div className="GradesComponentContainer">
+      <div className="GradesInformationTitle">Grade Information: </div>
+      {isError ? (
+        <div>Error loading grades.</div>
+      ) : isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="GradesContainer">
+            <table>
+              <tr>
+                <td>Average (All Time)</td>
+                <td>{gradesData?.average.toFixed(2)}</td>
+              </tr>
+              <tr>
+                <td>Average (5 Years)</td>
+                <td>{gradesData?.averageFiveYears.toFixed(2)}</td>
+              </tr>
+            </table>
+          </div>
+          <a
+            className="GradesLink"
+            href={gradesURL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View on UBC-Grades
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default GradesComponent;
--- src/content/SectionPopup/SectionPopup.tsx
+++ src/content/SectionPopup/SectionPopup.tsx
@@ -1,40 +1,51 @@
-import { ISectionData } from '../App/App.types'
-import { getGradesUrl } from '../Calendar/utils'
-import './SectionPopup.css'
+import { ISectionData } from "../App/App.types";
+import "./SectionPopup.css";
+import GradesComponent from "./GradesComponent/GradesComponent";
 
 interface IProps {
-  selectedSection: ISectionData,
-  sections: ISectionData[],
-  setSections: (data: ISectionData[]) => void,
-  setSelectedSection: (state: ISectionData | null) => void,
+  selectedSection: ISectionData;
+  sections: ISectionData[];
+  setSections: (data: ISectionData[]) => void;
+  setSelectedSection: (state: ISectionData | null) => void;
 }
 
-const SectionPopup = ({selectedSection, sections, setSections, setSelectedSection}:IProps) => {
+const SectionPopup = ({
+  selectedSection,
+  sections,
+  setSections,
+  setSelectedSection,
+}: IProps) => {
+  const removeSection = () => {
+    let updatedSections = [...sections];
+    updatedSections = updatedSections.filter(
+      (section) => section !== selectedSection
+    );
+    setSections(updatedSections);
+    setSelectedSection(null);
+  };
 
-    const removeSection = () => {
-        let updatedSections = [...sections]
-        updatedSections = updatedSections.filter((section) => section !== selectedSection)
-        setSections(updatedSections)
-        setSelectedSection(null)
-    }
-
-    const gradesURL = getGradesUrl(selectedSection)
-
-    return (
-        <div className="SectionPopup">
-            <div>
-                <div className='SectionPopupTitle'>{selectedSection?.code}</div>
-                <hr />
-                <div className='SectionPopupDetails'>{selectedSection?.name}</div>
-                <div className='SectionPopupDetails'>{selectedSection?.location}</div>
-                <a href={gradesURL} target="_blank">View grades</a>
-            </div>
-            <div className='SectionPopupButtonContainer'>
-                <button className='SectionPopupCancelButton' onClick={() => setSelectedSection(null)}>Close</button>
-                <button className='SectionPopupButton' onClick={removeSection}>Remove</button>
-            </div>
-        </div>
+  return (
+    <div className="SectionPopup">
+      <div>
+        <div className="SectionPopupTitle">{selectedSection?.code}</div>
+        <hr />
+        <div className="SectionPopupDetails">{selectedSection?.name}</div>
+        <div className="SectionPopupDetails">{selectedSection?.location}</div>
+        <GradesComponent selectedSection={selectedSection} />
+      </div>
+      <div className="SectionPopupButtonContainer">
+        <button
+          className="SectionPopupCancelButton"
+          onClick={() => setSelectedSection(null)}
+        >
+          Close
+        </button>
+        <button className="SectionPopupButton" onClick={removeSection}>
+          Remove
+        </button>
+      </div>
+    </div>
   );
-}
+};
 
-export default SectionPopup
+export default SectionPopup;
