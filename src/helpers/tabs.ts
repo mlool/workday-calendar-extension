2004fd0fabdfb00a5b99f6209fdc3d78f4cf0c88
Jaskirat Gill <gilljaskirat04@gmail.com>
2024-06-19 20:21:57 -0700

    delete more obsolete code

--- src/helpers/tabs.ts
+++
@@ -1,7 +0,0 @@
-import { tabs } from "webextension-polyfill"
-
-export async function getCurrentTab() {
-  const list = await tabs.query({ active: true, currentWindow: true })
-
-  return list[0]
-}
