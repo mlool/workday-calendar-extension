cd9c6d7f5f657379db58ee6f713609ef8271b12f
Jaskirat Gill <gilljaskirat04@gmail.com>
2024-06-20 19:40:28 -0700

    feat: Add fetchSecureToken function for session authentication

--- src/content/App/App.tsx
+++ src/content/App/App.tsx
@@ -11,7 +11,9 @@ import {
   getNewSectionColor,
 } from "../../helpers/courseColors"
 import { ModalLayer } from "../ModalLayer"
-import { filterSectionsByWorklist } from "../utils"
+import { filterSectionsByWorklist, findCourseInfo } from "../utils"
+
+export let sessionSecureToken: string | null = null
 
 function App() {
   const [newSection, setNewSection] = useState<ISectionData | null>(null)
@@ -28,6 +30,41 @@ function App() {
   // const prevSections = useRef(sections);
   // Sync initial state with chrome storage on mount
   useEffect(() => {
+    const fetchSecureToken = async () => {
+      const headers = new Headers({
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Session-Secure-Token": "",
+      });
+    
+      const urlencoded = new URLSearchParams();
+    
+      const requestOptions = {
+        method: "POST",
+        body: urlencoded,
+        redirect: "follow" as RequestRedirect,
+        headers: headers
+      };
+
+      return fetch(
+        "https://wd10.myworkday.com/ubc/app-root",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          try {
+            sessionSecureToken = data["sessionSecureToken"]
+          } catch (error) {
+            console.error("Error parsing data:", error)
+            return null
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching course data:", error)
+          return null
+        })
+    }
+
     const syncInitialState = () => {
       chrome.storage.local.get(
         ["currentTerm", "colorTheme", "sections", "currentWorklistNumber"],
@@ -66,8 +103,10 @@ function App() {
         }
       }
     }
-
+    // Set context-id
+    findCourseInfo("CPSC_V 320-101")
     syncInitialState()
+    fetchSecureToken()
     chrome.storage.onChanged.addListener(handleStorageChange)
 
     return () => {
