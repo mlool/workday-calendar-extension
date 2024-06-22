1b4009e97e174b2b5dad99767a86d5e41b89e563
Callum O'Riley <callumchristopheroriley@gmail.com>
2024-06-22 13:26:53 -0700

    Making the handling process for course not found error in the import schedule code a little better - continuing to next iteration of loop

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -580,6 +580,7 @@ async function handleCopyScheduleButtonClick(
 
     if (!selectedSection) {
       console.error("Unable to retrieve selected section")
+      continue
     }
     // Getting existing sections from Chrome storage and adding the new section
     chrome.storage.local.set({ newSection: selectedSection })
