397c9a5b34214e77a5daecb6114a01c067a51f02
Mlool <martinlou2002@gmail.com>
2024-06-29 14:27:21 -0700

    updated 2 term bug

--- src/content/CalendarContainer/CalendarContainer.tsx
+++ src/content/CalendarContainer/CalendarContainer.tsx
@@ -1,6 +1,7 @@
 import TabBar from "../../components/TabBar"
 import { ISectionData, Term_String_Map, Term } from "../App/App.types"
 import Calendar from "../Calendar/Calendar"
+import { convertToMatrix } from "../Calendar/calendarHelpers"
 import "./CalendarContainer.css"
 
 interface IProps {
@@ -18,7 +19,6 @@ const CalendarContainer = ({
   sections,
   newSection,
   currentWorklistNumber,
-  setSections,
   setSectionConflict,
   setCurrentWorklistNumber,
   currentTerm,
@@ -26,6 +26,37 @@ const CalendarContainer = ({
 }: IProps) => {
   const WORKLISTCOUNT = [0, 1, 2, 3]
   const TERMS = [Term.winterOne, Term.winterTwo]
+
+  const getSectionMatrix = (term: Term) => {
+    const calendarSectionsTermOne = sections.filter(
+      (section) =>
+        section.worklistNumber === currentWorklistNumber &&
+        (section.term === Term.winterOne || section.term === Term.winterFull)
+    )
+
+    const calendarSectionsTermTwo = sections.filter(
+      (section) =>
+        section.worklistNumber === currentWorklistNumber &&
+        (section.term === Term.winterTwo || section.term === Term.winterFull)
+    )
+
+    const sectionsToRenderTermOne = convertToMatrix(
+      calendarSectionsTermOne,
+      newSection,
+      Term.winterOne
+    )
+
+    const sectionsToRenderTermTwo = convertToMatrix(
+      calendarSectionsTermTwo,
+      newSection,
+      Term.winterTwo
+    )
+
+    setSectionConflict(sectionsToRenderTermOne[0] || sectionsToRenderTermTwo[0])
+    return term === Term.winterOne
+      ? sectionsToRenderTermOne[1]
+      : sectionsToRenderTermTwo[1]
+  }
 
   const canSwitchTerms = (): boolean => {
     if (newSection !== null && newSection.term !== Term.winterFull) return false
@@ -53,14 +84,7 @@ const CalendarContainer = ({
         />
       </div>
 
-      <Calendar
-        sections={sections}
-        currentWorklistNumber={currentWorklistNumber}
-        newSection={newSection}
-        setSections={setSections}
-        setSectionConflict={setSectionConflict}
-        currentTerm={currentTerm}
-      />
+      <Calendar sectionsToRender={getSectionMatrix(currentTerm)} />
     </div>
   )
 }
