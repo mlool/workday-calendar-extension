3653ece95230a0c5dd803d726066f44eb24c9039
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2024-07-31 21:43:27 -0700

    refactor: switch-based automigrator fn

--- src/storage/sectionStorage.ts
+++ src/storage/sectionStorage.ts
@@ -6,8 +6,11 @@ import {
 } from "../content/Settings/Theme/courseColors"
 import {
   v1_4_1,
+  v1_4_1_SectionData,
   v1_5_0,
+  v1_5_0_SectionData,
   v2_0_0,
+  v2_0_0_SectionData,
   ValidVersionData,
 } from "./legacyStorageMigrators"
 
@@ -27,12 +30,32 @@ const readSectionData = async (): Promise<ISectionData[]> => {
   return assignColors(processedSections, ColorTheme.Green)
 }
 
-// TODO: is there a clean, mostly type-safe, non-recursive way of doing this?
-// switch statement + fallthru would work but requires a lot of type casting
 const sectionDataAutoMigrator = async (
   version: string,
   oldSections: ValidVersionData
 ): Promise<ISectionData[]> => {
+  switch (version) {
+    case "1.4.1":
+      oldSections = await v1_4_1(oldSections as v1_4_1_SectionData[])
+      version = "1.6.0"
+    // fall through
+    case "1.5.0":
+      oldSections = v1_5_0(oldSections as v1_5_0_SectionData[])
+      version = "1.6.0"
+    // fall through
+    case "1.6.0":
+    case "2.0.0":
+      oldSections = v2_0_0(oldSections as v2_0_0_SectionData[])
+      version = "2.0.1"
+    // fall through
+    case "2.0.1":
+      return oldSections as ISectionData[]
+    default:
+      throw Error(
+        `Version ${version} could not be properly migrated! Are you sure this version of the extension supports this format?`
+      )
+  }
+  /*
   // base case
   if (version === "2.0.1") return oldSections as ISectionData[]
 
@@ -51,6 +74,7 @@ const sectionDataAutoMigrator = async (
   throw Error(
     `Version ${version} could not be properly migrated! Are you sure this version of the extension supports this format?`
   )
+  */
 }
 
 const writeSectionData = async (newSections: ISectionData[]) => {
