5c5ad536fe46d0f9d6886112e698ea528f1f813e
Mlool <martinlou2002@gmail.com>
2026-01-07 11:41:25 -0800

    Updated section detail for more external tools

--- src/content/SectionDetails/SectionDetails.tsx
+++ src/content/SectionDetails/SectionDetails.tsx
@@ -4,6 +4,9 @@ import { IGradesAPIData } from "../../objects/Section"
 import "./SectionDetails.css"
 import CloseIcon from "../Icons/CloseIcon";
 import ExternalLinkIcon from "../Icons/ExternalLinkIcon";
+import { RMPData } from "../../backends/rateMyProf";
+import Browser from "webextension-polyfill"
+
 
 interface IProps {
     section: Section;
@@ -45,12 +48,60 @@ const SectionDetails = ({ section, term, onClose, onDelete }: IProps) => {
 }
 
 const InstructorComponent = ({ section }: { section: Section }) => {
+    const [rmpRatings, setRmpRatings] = useState<Record<string, RMPData | null>>()
+    useEffect(() => {
+        populateRmpRatings()
+    }, [])
+
+    const populateRmpRatings = async () => {
+        const fetches = []
+        const instructors = section.getInstructors()
+        const isVancouver = section.getCode().includes("_V")
+        for (const prof of instructors) {
+            fetches.push(
+                // cannot directly fetch from RMP here - fetching
+                // from the service worker with the right
+                // host_permissions allows us to bypass CORS.
+                Browser.runtime.sendMessage({
+                    type: "RMP",
+                    prof: prof,
+                    isVancouver: isVancouver,
+                })
+            )
+        }
+        const results = await Promise.all(fetches)
+        const ratings = results.reduce<Record<string, RMPData>>(
+            (acc, item, index) => {
+                acc[instructors[index]] = item
+                return acc
+            },
+            {}
+        )
+        setRmpRatings(ratings)
+    }
+
+    const Rating = ({ instructor, rating }: { instructor: string, rating: number | undefined }) => {
+        return <div className="detail-link">
+            {rating ? rating + "/5" : "N/A"}
+            <ExternalLinkIcon size={14} href={rmpRatings?.[instructor]?.link ?? `https://www.google.com/search?q=${instructor + " UBC"}`} />
+        </div>
+    }
+
     return (
-        <div className="detail-row">
-            <span className="detail-label">Instructors:</span>
-            <span className="detail-value">
-                {section.getInstructors().length > 0 ? section.getInstructors().join(", ") : "No instructor found."}
-            </span>
+        <div className="detail-section">
+            <div className="detail-header">Instructors</div>
+            {section.getInstructors().length > 0 ? (
+                section.getInstructors().map((instructor, index) => (
+                    <div className="detail-row" key={index}>
+                        <span className="detail-value">{instructor}</span>
+                        <Rating instructor={instructor} rating={rmpRatings?.[instructor]?.rating} />
+                    </div>
+                ))
+            ) : (
+                <div className="detail-row">
+                    <span className="detail-value">No instructor found.</span>
+                </div>
+            )}
         </div>
     )
 }
@@ -67,30 +118,44 @@ const GradeComponent = ({ section }: { section: Section }) => {
     }
 
     return (
-        <div className="detail-row">
-            <span className="detail-label">Grades:</span>
-            {grades && <div style={{ display: "flex", flexDirection: "column" }}>
-                <span className="detail-value">
-                    {
-                        grades?.average ? grades.average.toFixed(2) + " (avg)" : "Data Not Available"
-                    }
-                </span>
-                <span className="detail-value">
-                    {
-                        grades?.averageFiveYears ? grades.averageFiveYears.toFixed(2) + " (avg past 5 years)" : "Data Not Available"
-                    }
-                </span></div>}
+        <div className="detail-section">
+            <div className="detail-header">Grades</div>
+            {grades ? (
+                <>
+                    <div className="detail-row">
+                        <span className="detail-value">Average (All Years)</span>
+                        <div className="detail-link">{grades.average ? grades.average.toFixed(2) + "%" : "N/A"} <ExternalLinkIcon size={14} href={section.getGradesUrl()} /></div>
+                    </div>
+                    <div className="detail-row">
+                        <span className="detail-value">Average (5 Years)</span>
+                        <div className="detail-link">{grades.averageFiveYears ? grades.averageFiveYears.toFixed(2) + "%" : "N/A"} <ExternalLinkIcon size={14} href={section.getGradesUrl()} /></div>
+                    </div>
+                </>
+            ) : (
+                <div className="detail-row">
+                    <span className="detail-value">No historical grades found.</span>
+                </div>
+            )}
         </div>
     )
 }
 
 const LocationComponent = ({ section, term }: { section: Section; term: number }) => {
+    const locations = section.getLocations([term]);
     return (
-        <div className="detail-row">
-            <span className="detail-label">Location:</span>
-            <span className="detail-value">
-                {section.getLocations([term]).length > 0 ? section.getLocations([term]).join(", ") : "No location found."}
-            </span>
+        <div className="detail-section">
+            <div className="detail-header">Location</div>
+            {locations.length > 0 ? (
+                locations.map((loc, index) => (
+                    <div className="detail-row" key={index}>
+                        <span className="detail-value" style={{ flex: 2 }}>{loc}</span>
+                    </div>
+                ))
+            ) : (
+                <div className="detail-row">
+                    <span className="detail-value">No location found.</span>
+                </div>
+            )}
         </div>
     )
 }
